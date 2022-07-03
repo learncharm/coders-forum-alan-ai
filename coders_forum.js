7e79f8499148993160c5562e191d638c9d40616f
Krupesh Vithlani <76093323+tkrupesh14@users.noreply.github.com>
2022-07-03 22:22:27 +0530

    beta

--- coders_forum.js
+++ coders_forum.js
@@ -2,3 +2,11 @@
 intent('hello world', p => {
     p.play('(hello|hi there)');
 });
+intent('e beta! admin panel kholo zara', (p) => {
+    p.play('Admin Panel Opeaning, Thanks for using Learncharm');
+    p.play({command:'adminPanel'});
+   
+});
+intent('Open Home Page', (p) => {
+    p.play({command:'homePage'});
+});
