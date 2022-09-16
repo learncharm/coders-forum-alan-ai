6e44b9890677818a3cc6c14bd93d148277ef8f03
Krupesh Vithlani <76093323+tkrupesh14@users.noreply.github.com>
2022-09-16 22:09:11 +0530

    admin ai done

--- coders_forum.js
+++ coders_forum.js
@@ -2,11 +2,30 @@
 intent('hello world', p => {
     p.play('(hello|hi there)');
 });
-intent('e beta! admin panel kholo zara', (p) => {
+// admin side
+intent('go to admin panel', (p) => {
     p.play('Admin Panel Opeaning, Thanks for using Learncharm');
     p.play({command:'adminPanel'});
    
 });
-intent('Open Home Page', (p) => {
+intent('Go to Admin Threads', (p) => {
+    p.play('Going to Admin Threads');
+    p.play({command:'adminThreads'});
+});
+intent('Go to Admin Category', (p) => {
+    p.play('Going to Admin Category');
+    p.play({command:'adminCategory'});
+});
+intent('Go to Admin Answers', (p) => {
+    p.play('Going to Admin Comments');
+    p.play({command:'adminComments'});
+});
+intent('Go to Admin Users', (p) => {
+    p.play('Going to Admin Users');
+    p.play({command:'adminUsers'});
+});
+
+intent('Go to Home Page', (p) => {
+    p.play('Going to Home Page');
     p.play({command:'homePage'});
 });
